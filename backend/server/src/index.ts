f8880b2b263e5eb7558a493b317265c01775a411
sivat_r <its-sivat_r@internal.softsquaregroup.com>
2026-03-31 16:37:18 +0700

    Resolve limit file size

--- backend/server/src/index.ts
+++ backend/server/src/index.ts
@@ -17,9 +17,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(cors());
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+app.use(cors());
 
 app.use((req, res, next) => {
   res.setHeader('ngrok-skip-browser-warning', 'true');
