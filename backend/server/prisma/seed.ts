0f21b237564fdec6628308274f0834f20e80974c
sivat_r <its-sivat_r@internal.softsquaregroup.com>
2026-03-31 15:37:51 +0700

    change API_BASE_URL

---
+++ backend/server/prisma/seed.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  console.log('เริ่มการ Seed ข้อมูล...')
+  
+
+  const roomTypes = [
+    { name: 'ห้องพัดลม' },
+    { name: 'ห้องแอร์' },
+    { name: 'ห้องแอร์ (VIP)' }
+  ]
+
+  for (const type of roomTypes) {
+    await prisma.roomType.upsert({
+      where: { name: type.name },
+      update: {},
+      create: {
+        name: type.name,
+      },
+    })
+  }
+
+  console.log('Seed ข้อมูลประเภทห้องสำเร็จ!')
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
