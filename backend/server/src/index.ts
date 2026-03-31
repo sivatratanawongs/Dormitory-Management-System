15517c71d55968c6a10de413b87d7179e0381663
sivat_r <its-sivat_r@internal.softsquaregroup.com>
2026-03-31 16:29:42 +0700

    Resolve limit file size

--- backend/server/src/index.ts
+++ backend/server/src/index.ts
@@ -18,32 +18,27 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(cors());
-app.use(express.json());
-
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 app.use((req, res, next) => {
   res.setHeader('ngrok-skip-browser-warning', 'true');
   next();
 });
-const rootDir = path.resolve(__dirname, '..'); 
-app.use('/uploads', express.static(path.join(rootDir, 'uploads')));
+
+app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
 // --- API Routes ---
 app.use('/api/line', lineRoutes);
 app.use('/api/settings', settingRoutes);
 app.use('/api/rooms', roomRoutes); 
-app.use('/api/billings',billingRoutes)
-app.use('/api/tenants',tenantRoutes)
-app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
+app.use('/api/billings', billingRoutes);
+app.use('/api/tenants', tenantRoutes);
 
 app.get('/', (_req: Request, res: Response) => {
   res.send('🏠 Baan Jatuporn API Server is running...');
 });
 
 app.listen(Number(PORT), '0.0.0.0', () => {
-  console.log(`
-  API Server is ready!
-  URL: http://localhost:${PORT}
-  `);
+  console.log(`API Server is ready! URL: http://localhost:${PORT}`);
 });
